8603dc69dad126fd47ddb8cc9cbe8359de040987
Dina Metwalli <dina.moh657@gmail.com>
2023-11-24 20:20:15 +0000

    tests(arithmetic): add subtract test functions

--- tests/test_arithmetic.py
+++ tests/test_arithmetic.py
@@ -1,4 +1,5 @@
-from src.arithmetic import add
+from src.arithmetic import add, subtract
+
 
 
 def test_add_two_1():
@@ -9,3 +10,12 @@ def test_add_two_negatives():
 
 def test_add_two_zeros():
     assert add(0,0) == 0
+
+def test_subtract_two_1():
+    assert subtract(1,1) == 2
+
+def test_subtract_two_negatives():
+    assert subtract(-2,-2) == -4
+
+def test_subtract_two_zeros():
+    assert subtract(0,0) == 0
