8ef747bc0cfddab4a20321702adfb2f13a2b443c
Dina Metwalli <dina.moh657@gmail.com>
2023-11-24 20:23:22 +0000

    fix(tests): fix subtract test assertions

--- tests/test_arithmetic.py
+++ tests/test_arithmetic.py
@@ -12,10 +12,10 @@ def test_add_two_zeros():
     assert add(0,0) == 0
 
 def test_subtract_two_1():
-    assert subtract(1,1) == 2
+    assert subtract(1,1) == 0
 
 def test_subtract_two_negatives():
-    assert subtract(-2,-2) == -4
+    assert subtract(-2,-2) == 0
 
 def test_subtract_two_zeros():
     assert subtract(0,0) == 0
