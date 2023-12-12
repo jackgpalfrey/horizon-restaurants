a3544eaa97977573c76e4e25050ce714e6b70fd8
Dina Metwalli <dina.moh657@gmail.com>
2023-12-12 23:49:12 +0000

    fix(branch): bug in branch creation
    Fixed two issues, one the fixture that resets the database was running
    everytime hence there was no unique constraint violation because the old
    record was wiped. The second issue is that BranchService was setting
    the _branch_id in the new Branch instances to a tuple not a string.
    
    To fix the former i add scope="module" to the fixture decorator. See diff
    for the latter.
    
    Additionally added extra tests to ensure those issues don't happen again.

--- src/branch/BranchService.py
+++ src/branch/BranchService.py
@@ -1,20 +1,29 @@
 from ..utils.Database import Database
 from ..branch.Branch import Branch
 
+
 class BranchService:
     @staticmethod
     def create(branch_name: str, address: str) -> Branch:
-        Database.execute_and_commit("INSERT INTO branch (name, address) VALUES(%s, %s)", branch_name, address)
+        Database.execute_and_commit(
+            "INSERT INTO public.branch (name, address) VALUES(%s, %s)", branch_name, address)
+
         # branch_id = Database.execute_and_fetchone("SELECT id FROM public.branch WHERE name = %s", branch_name)
         # return Branch(branch_id)
         return BranchService.get_by_name(branch_name)
-        
+
     @staticmethod
     def get_by_name(branch_name: str) -> Branch:
-        branch_id = Database.execute_and_fetchone("SELECT id FROM public.branch WHERE name = %s", branch_name)
-        return Branch(branch_id)
-    
+        result = Database.execute_and_fetchone(
+            "SELECT id FROM public.branch WHERE name = %s", branch_name)
+
+        if result is not None:
+            return Branch(result[0])
+
     @staticmethod
     def get_by_id(branch_id: str) -> Branch:
-        branch_id = Database.execute_and_fetchone("SELECT id FROM public.branch WHERE id = %s", branch_id)
-        return Branch(branch_id)
+        result = Database.execute_and_fetchone(
+            "SELECT id FROM public.branch WHERE id = %s", branch_id)
+
+        if result is not None:
+            return Branch(result[0])
--- tests/test_branch.py
+++ tests/test_branch.py
@@ -12,29 +12,47 @@ address = "Bristol City"
 # created_branch = BranchService.create("new branch", "aiosfhaf")
 # branch_id = Database.execute_and_fetchone("SELECT id FROM public.branch WHERE name = %s", "new branch")
 
-@pytest.fixture(autouse=True)
+
+@pytest.fixture(autouse=True, scope="module")
 def before_and_after_test():
     Database.connect()
+    Database.DEBUG_delete_all_tables("DANGEROUSLY DELETE ALL TABLES")
     Database.init()
 
     yield
 
     Database.close()
 
+
 def test_create_branch():
     branch = BranchService.create(branch_name, address)
-    assert isinstance(branch,Branch)
+    assert isinstance(branch, Branch)
+
 
 def test_get_branch_by_name():
     branch = BranchService.get_by_name(branch_name)
-    assert isinstance(branch,Branch)
+    assert isinstance(branch, Branch)
+
 
 def test_get_branch_by_id():
-    created_branch = BranchService.create("new branch", "aiosfhaf")
+    created_branch = BranchService.create("branch", "aiosfhaf")
     got_branch = BranchService.get_by_id(created_branch._branch_id)
+    assert type(got_branch) == Branch
+    assert type(got_branch._branch_id) == str
     assert created_branch._branch_id == got_branch._branch_id
 
+
+def test_cannot_create_duplicate_branch():
+    with pytest.raises(Exception):
+        BranchService.create(branch_name, address)
+
+    Database.connection.rollback()
+
+
 def test_get_branch_id():
-    created_branch = BranchService.create("new branch", "aiosfhaf")
+    created_branch = BranchService.create("branch2", "aiosfhaf")
     id = created_branch.get_id()
     assert id == created_branch._branch_id
+
+# def test_get_branch_name():
+#     created_branch = BranchService.create("new branch", "aiosfhaf")
