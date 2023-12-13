c9d5645a43effc94003fc8bdc786f4cfcc940b79
Jack Palfrey <jackgpalfrey@outlook.com>
2023-12-13 17:47:40 +0000

    refactor(users): for readability
    Adds docstrings and general cleanup to the Role class.

--- src/user/Role.py
+++ src/user/Role.py
@@ -8,21 +8,32 @@ ROLE_DIR_PATH = "src/config/roles"
 class Role:
     _roles: dict[int, Role] = {}
 
-    _role_id: int
-    _name: str
-    _permissions: dict[str, bool]
-
     @classmethod
     def load_roles(cls):
-        dir = list(os.scandir(ROLE_DIR_PATH))
-        sorted_dir = sorted(dir, key=lambda entry: entry.name)
+        """
+        Loads all roles from src/config/roles/
 
-        for entry in sorted_dir:
-            if entry.is_file():
-                Role._load_role_file(entry.path)
+        :raises FileNotFoundError: If the role directory does not exist
+        """
+
+        files = os.scandir(ROLE_DIR_PATH)
+        sorted_files = sorted(files, key=lambda entry: entry.name)
+
+        for file in sorted_files:
+            if file.is_file():
+                Role._load_role_file(file.path)
+            else:
+                print("Skipping directory:", file.name)
 
     @classmethod
     def _load_role_file(cls, path: str):
+        """
+        Loads a yaml file at the given path as a role
+
+        :raises FileNotFoundError: If the role file does not exist
+        :raises KeyError: If the role file is missing any required fields
+        """
+
         print("Loading role file:", path, end="... ")
 
         with open(path, "r") as f:
@@ -34,14 +45,7 @@ class Role:
         extends = data.get("extends", None)
 
         if extends is not None:
-            if type(extends) is list:
-                for parent_role in extends:
-                    parent_role = cls.get_by_id(parent_role)
-                    permissions += parent_role._permissions.keys()
-
-            else:
-                parent_role = cls.get_by_id(extends)
-                permissions += parent_role._permissions.keys()
+            cls._handle_extends(extends, permissions)
 
         role = Role(id, name, permissions)
         cls._roles[id] = role
@@ -49,38 +53,63 @@ class Role:
         print("Loaded", name)
 
     @classmethod
+    def _handle_extends(cls, extends: list[int] | int, permissions: list[str]):
+        if type(extends) is list:
+            for role_id in extends:
+                parent_role = cls.get_by_id(role_id)
+                permissions += parent_role.get_all_permissions()
+
+        else:
+            parent_role = cls.get_by_id(extends)
+            permissions += parent_role.get_all_permissions()
+
+    @classmethod
     def get_by_id(cls, id: int) -> Role | None:
         return cls._roles.get(id, None)
 
-        raise ValueError(f"Role with id {id} does not exist")
+    # ---------------------------------- Instance Methods ----------------------------------
 
     def __init__(self, id: int, name: str, permissions: list[str]):
         self._role_id = id
         self._name = name
-        self._permissions = {}
+        self._permissions: dict[str: bool] = {}
 
         self._add_permission_list(permissions)
 
     def check_permission(self, permission: str) -> bool:
+        """
+        Checks if this role has the given permission
+
+        Supports implicit wildcards, e.g. "account.create" will come back True if this role has "account"
+        Supports explicit wildcards, i.e. any permission check will come back True if this role has the ".*" permission
+        Supports negation with the ~ prefix, e.g. "account.create" will come back False if this role has "account" but also "~account.create"
+        """
+
         split_permission = permission.split(".")
 
         return self._check_split_permission(split_permission)
 
     def _check_split_permission(self, split_permission: list[str]) -> bool:
-        if len(split_permission) == 1:
-            # Handle explict wildcard
-            if self._permissions.get(".*", False):
-                return True
+        """
+        Recursively checks if this role has the given permission to support implicit wildcards
+        """
 
-            return self._permissions.get(split_permission[0], False)
+        # Base Case
+        if len(split_permission) == 1:
+            HAS_PERMISSION = self._check_exact_permission(split_permission[0])
+
+            if HAS_PERMISSION is not None:
+                return HAS_PERMISSION
+
+            return self._check_exact_permission(".*") or False
 
         permission_str = ".".join(split_permission)
-        result = self._permissions.get(permission_str, None)
+        permission_value = self._check_exact_permission(permission_str)
 
-        if result is None:
+        if permission_value is None:
             return self._check_split_permission(split_permission[:-1])
 
-        return result
+        return permission_value
 
     def get_name(self) -> str:
         return self._name
@@ -88,11 +117,29 @@ class Role:
     def get_id(self) -> int:
         return self._role_id
 
+    def get_all_permissions(self) -> list[str]:
+        return list(self._permissions.keys())
+
+    def _check_exact_permission(self, permission: str) -> bool | None:
+        """
+        Checks if this role has the given permission exactly. Does not support wildcards
+        """
+        perm = self._permissions.get(permission, None)
+        print(permission, perm)
+        return perm
+
     def _add_permission_list(self, permissions: list[str]):
+        """Adds a list of permissions to this role"""
+
         for permission in permissions:
             self._add_permission(permission)
 
     def _add_permission(self, permission: str):
+        """
+        Adds a permission to this role
+        Supports negation with the ~ operator
+        """
+
         if permission.startswith("~"):
             self._permissions[permission[1:]] = False
         else:
