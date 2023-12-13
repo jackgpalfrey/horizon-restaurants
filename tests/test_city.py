82d140ade8b0c5b6eb56d1afefd69cd60f07a028
Dina Metwalli <dina.moh657@gmail.com>
2023-12-13 22:11:29 +0000

    feat(city): add city table

---
+++ tests/test_city.py
@@ -0,0 +1,13 @@
+import pytest
+from src.utils.Database import Database
+
+
+@pytest.fixture(autouse=True, scope="module")
+def before_and_after_test():
+    Database.connect()
+    Database.DEBUG_delete_all_tables("DANGEROUSLY DELETE ALL TABLES")
+    Database.init()
+
+    yield
+
+    Database.close()
