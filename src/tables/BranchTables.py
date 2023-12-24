b570482e4efa426c0341eb5d75673fed73bfc0cf
Dina Metwalli <dina.moh657@gmail.com>
2023-12-24 00:36:53 +0000

    feat(tables): add BranchTables and Table classes

---
+++ src/tables/BranchTables.py
@@ -0,0 +1,8 @@
+from ..branch.Branch import Branch
+from .Table import Table
+from ..utils.Database import Database
+
+
+class BranchTables:
+    def __init__(self, branch_id: str):
+        self._branch_id = branch_id
---
+++ src/tables/Table.py
@@ -0,0 +1,3 @@
+class Table:
+    def __init__(self, table_id: str):
+        self._table_id = table_id
