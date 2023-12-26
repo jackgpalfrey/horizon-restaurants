0e28b1122591cec8b9953fd63bd530297254d5f4
Dina Metwalli <dina.moh657@gmail.com>
2023-12-26 18:41:43 +0000

    feat(reservations): add create method

--- src/reservations/BranchReservations.py
+++ src/reservations/BranchReservations.py
@@ -1,3 +1,32 @@
+from datetime import datetime, timedelta
+from .Reservation import Reservation
+from ..tables.Table import Table
+from ..user.ActiveUser import ActiveUser
+from ..utils.Database import Database
+
+
 class BranchReservations:
     def __init__(self, branch_id: str):
         self._branch_id = branch_id
+
+    def create(self, table: Table, customer_name: str, reservation_date: datetime, start_time: datetime, guest_num: int) -> Reservation:
+
+        ActiveUser.get().raise_without_permission("reservation.create")
+
+        table_id = table._table_id
+
+        # reference: https://blog.finxter.com/how-to-add-time-onto-a-datetime-object-in-python/
+
+        start_time = datetime.strptime(start_time, '%H:%M')
+        duration = timedelta(hours=2)
+        end_time = start_time + duration
+        start_time = start_time.strftime("%X")
+        end_time = end_time.strftime("%X")
+
+        cursor = Database.execute("INSERT INTO public.reservations(customer_name, reservation_date, start_time, end_time, guest_num, table_id, branch_id)VALUES(%s, %s, %s, %s, %s, %s, %s) RETURNING id",
+                                  customer_name, reservation_date, start_time, end_time, guest_num, table_id, self._branch_id)
+        Database.commit()
+        result = cursor.fetchone()
+        id = result[0]
+
+        return Reservation(id)
---
+++ tests/test_reservations.py
@@ -0,0 +1,37 @@
+import pytest
+from src.branch.BranchService import BranchService
+from src.city.CityService import CityService
+from src.reservations.Reservation import Reservation
+from src.user.UserService import UserService
+from src.utils.Database import Database
+
+customer_name = "Glenn Juarez"
+reservation_date = "2023-12-26"  # FORMAT YY/MM/DD
+start_time = "14:50"  # FORMAT HH:MM
+guest_num = 4
+
+
+@pytest.fixture(autouse=True, scope="module")
+def before_and_after_test():
+    Database.connect()
+    Database.DEBUG_delete_all_tables("DANGEROUSLY DELETE ALL TABLES")
+    Database.init()
+    UserService.init()
+
+    UserService.login("admin", "admin")
+
+    yield
+
+    Database.close()
+
+
+def test_create_reservation():
+    city = CityService.create("Bristol")
+    branch = BranchService.create(
+        "Bristol", "15-29 Union St, Bristol BS1 2DF", city)
+    branch_table = branch.tables()
+    table = branch_table.create(1, 4)
+    branch_reservation = branch.reservations()
+    reservation = branch_reservation.create(
+        table, customer_name, reservation_date, start_time, guest_num)
+    assert isinstance(reservation, Reservation)
