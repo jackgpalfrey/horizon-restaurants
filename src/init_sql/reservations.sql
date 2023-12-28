5965347802ff043fcbebc2638ff4eea8dd55ee6a
Dina Metwalli <dina.moh657@gmail.com>
2023-12-28 11:57:36 +0000

    fix(reservations): change end_time SQL data type

--- src/init_sql/reservations.sql
+++ src/init_sql/reservations.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS public.reservations(
     id uuid NOT NULL DEFAULT gen_random_uuid() UNIQUE,
     customer_name text NOT NULL,
     reservation_time TIMESTAMP NOT NULL,
-    end_time TIME NOT NULL,
+    end_time TIMESTAMP NOT NULL,
     guest_num integer NOT NULL,
     table_id uuid NOT NULL,
     branch_id uuid NOT NULL,
