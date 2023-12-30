772952d042661acf19782556d5b75abda58e2912
Dina Metwalli <dina.moh657@gmail.com>
2023-12-30 00:39:02 +0000

    fix(inventory): quantity and threshold datatypes

--- src/init_sql/8inventory.sql
+++ src/init_sql/8inventory.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS public.inventory(
     id uuid NOT NULL DEFAULT gen_random_uuid() UNIQUE,
     name text NOT NULL UNIQUE,
-    quantity TIMESTAMP NOT NULL,
-    threshold TIMESTAMP,
+    quantity integer NOT NULL,
+    threshold integer,
     branch_id uuid NOT NULL,
     PRIMARY KEY (id),
     CONSTRAINT fk_branch FOREIGN KEY (branch_id) REFERENCES public.branch(id) ON DELETE SET NULL
