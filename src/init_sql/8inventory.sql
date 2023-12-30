56d3e221c554b00924a93a511f9609270bf57d5a
Dina Metwalli <dina.moh657@gmail.com>
2023-12-30 00:05:14 +0000

    feat(inventory): add inventory table SQL

---
+++ src/init_sql/8inventory.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS public.inventory(
+    id uuid NOT NULL DEFAULT gen_random_uuid() UNIQUE,
+    name text NOT NULL UNIQUE,
+    quantity TIMESTAMP NOT NULL,
+    threshold TIMESTAMP,
+    branch_id uuid NOT NULL,
+    PRIMARY KEY (id),
+    CONSTRAINT fk_branch FOREIGN KEY (branch_id) REFERENCES public.branch(id) ON DELETE SET NULL
+)
