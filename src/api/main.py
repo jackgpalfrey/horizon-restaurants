f3b7aafa5cedd12d4697d3c1bddba9697902f6b5
Jack Palfrey <jackgpalfrey@outlook.com>
2024-01-04 15:08:20 +0000

    feat(api): add sessions

--- src/api/main.py
+++ src/api/main.py
@@ -2,11 +2,16 @@
 from flask import Flask
 from .utils.FSRouter import FSRouter
 
+SESSION_KEY = "joe"
 
 def main():
     """Run main entry point for api."""
     print("Starting API...")
     app: Flask = Flask("Horizon Restaurants")
+    app.secret_key = SESSION_KEY
+
+    # If you want sessions to last longer than a browser sessions
+    # app.config["PERMANENT_SESSION_LIFETIME"] = datetime.timedelta(days=3)
 
     router = FSRouter(app)
     router.load_dir("src/api/routes")
--- src/api/routes/login.py
+++ src/api/routes/login.py
@@ -1,3 +1,4 @@
+from flask import session
 from marshmallow import Schema, fields
 
 from src.api.utils.Result import Error, OK, Status
@@ -15,6 +16,8 @@ def post(body: dict):
     username: str = body["username"]
     password: str = body["password"]
 
+    print(session.get("user_id"), flush=True)
+
     try:
         UserService.login(username, password)
     except AuthenticationError:
@@ -22,7 +25,13 @@ def post(body: dict):
                      "Incorrect credentials")
 
     user = ActiveUser.get()
+    user_id = user.get_id()
+    role_id = user.get_role().get_id()
+
+    session["user_id"] = user_id
+    ActiveUser.clear()
+
     return OK({
-        "user_id": user.get_id(),
-        "role_id": user.get_role().get_id()
+        "user_id": user_id,
+        "role_id": role_id
     })
