e736f438d61ad020519b9887068ac017bb41bdc0
Dina Metwalli <dina.moh657@gmail.com>
2024-01-04 14:13:39 +0000

    feat(GUI): add login page
    - create Login.py
    - add Page.py and Pagemaker.py for page management
    - add link between login page and main application

---
+++ gui/Login.py
@@ -0,0 +1,13 @@
+from lib import Page
+import tkinter as tk
+
+
+class LoginPage(Page):
+    def __init__(self, *args, **kwargs):
+        Page.__init__(self, *args, **kwargs)
+        label = tk.Label(self, text="This is the login page")
+        label.pack()
+
+        btn = tk.Button(self, text="Login",
+                        command=lambda: self.pages.goto("loggedin"))
+        btn.pack()
--- gui/main.py
+++ gui/main.py
@@ -1,11 +1,34 @@
 import tkinter as tk
+from lib import Page, PageManager
+
+from Login import LoginPage
 
 
-class App(tk.Tk):
-    def __init__(self):
-        super().__init__()
+class App(Page):
+    def __init__(self, *args, **kwargs):
+        Page.__init__(self, *args, **kwargs)
+        root.geometry("675x300")
+        root.title('Horizon Restaurants')
+        root.resizable(0, 0)
+        root.columnconfigure(0, weight=1)
+        root.columnconfigure(1, weight=3)
+
+        label = tk.Label(self, text="This is the main page")
+        label.pack()
+
+        btn = tk.Button(self, text="Logout",
+                        command=lambda: self.pages.goto("loggedout"))
+        btn.pack()
 
 
-if __name__ == '__main__':
-    app = App()
-    app.mainloop()
+if __name__ == "__main__":
+    root = tk.Tk()
+    main = PageManager(root)
+    main.pack(side="top", fill="both", expand=True)
+
+    main.add_page("loggedout", LoginPage)
+    main.add_page("loggedin", App)
+
+    main.goto("loggedout")
+
+    root.mainloop()
