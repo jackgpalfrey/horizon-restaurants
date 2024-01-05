458ecd341aeeed407d67582f9f89977287ddad7a
Dina Metwalli <dina.moh657@gmail.com>
2024-01-05 21:53:10 +0000

    feat(GUI): add ChooseBranch page and widgets
    - add Login page widgets
    - add input validation for login/

--- gui/Login.py
+++ gui/Login.py
@@ -2,47 +2,78 @@ from gui_lib import Page
 import tkinter as tk
 from tkinter import *
 from tkinter import ttk
+from api import URL, API, State
 
 
 class ChooseBranch(Page):
     def __init__(self, *args, **kwargs):
         Page.__init__(self, *args, **kwargs)
 
-        dropdown = [
-            "Branch"
-        ]
+        label = tk.Label(self, text="Please choose a branch")
+        label.grid()
 
+        dropdown = []
+
+        all_branches_res = API.post(f"{URL}/branches")
+        all_branches = all_branches_res.json()
+
+        global branch_data
+        branch_data = {}
+        for branch in all_branches["data"]["branches"]:
+            dropdown.append(branch["name"])
+            data = {branch["name"]: branch["id"]}
+            branch_data.update(data)
+
+        global clicked
         clicked = StringVar()
-        clicked.set("Choose a Branch")
-
+        display_text = dropdown[0]
+        clicked.set(display_text)
         drop = OptionMenu(self, clicked, *dropdown)
-        drop.pack()
-
+        drop.grid()
         btn = tk.Button(self, text="Choose Branch",
                         command=lambda: self.pages.goto("login"))
-        btn.pack(anchor=tk.W, padx=10, pady=5, fill=tk.X, expand=True)
+        btn.grid()
 
 
 class LoginPage(Page):
     def __init__(self, *args, **kwargs):
         Page.__init__(self, *args, **kwargs)
-        label = tk.Label(self, text="This is the login page")
-        label.pack()
         self.create_widgets()
 
     def create_widgets(self):
-        username = tk.StringVar()
-        password = tk.StringVar()
+        self.username = tk.StringVar()
+        self.password = tk.StringVar()
+        username_label = ttk.Label(self, text="Username:").grid(
+            column=0, row=0, sticky=tk.W, padx=5, pady=5)
+        username_entry = ttk.Entry(self, textvariable=self.username).grid(
+            column=1, row=0, sticky=tk.W, padx=5, pady=5)
+        password_label = ttk.Label(self, text="Password:").grid(
+            column=0, row=1, sticky=tk.W, padx=5, pady=5)
+        password_entry = ttk.Entry(
+            self, textvariable=self.password, show="*").grid(column=1, row=1, sticky=tk.W, padx=5, pady=5)
 
-        fields = {}  # dictionary
-        fields['username_label'] = ttk.Label(self, text='Username:')
-        fields['username'] = ttk.Entry(self, textvariable=username)
-        fields['password_label'] = ttk.Label(self, text='Password:')
-        fields['password'] = ttk.Entry(self, textvariable=password, show="*")
+        btn = tk.Button(self, text="Login", command=self.handle_input)
+        btn.grid(column=0, row=2, sticky=tk.W, padx=5)
+        self.label = Label(self, text="")
+        self.label.grid(column=1, row=2)
 
-        for field in fields.values():
-            field.pack(anchor=tk.W, padx=10, pady=5, fill=tk.X, expand=True)
+    def handle_input(self):
+        branch_id = branch_data[clicked.get()]
+        branch_users_res = API.post(f"{URL}/branches/{branch_id}/users")
+        branch_users = branch_users_res.json()
 
-        btn = tk.Button(self, text="Login",
-                        command=lambda: self.pages.goto("loggedin"))
-        btn.pack(anchor=tk.W, padx=10, pady=5, fill=tk.X, expand=True)
+        if self.username.get() != "admin":
+            for user in branch_users["data"]["users"]:
+                if user["username"] == self.username.get():
+                    break
+                print("This user doesn't work at this branch.")
+
+        login_data = {"username": self.username.get(
+        ), "password": self.password.get()}
+        login = API.post(f"{URL}/login", json=login_data)
+        match login.status_code:
+            case 200:
+                self.label["text"] = ""
+                self.pages.goto("loggedin")
+            case 401:
+                self.label["text"] = "Invalid Credentials"
