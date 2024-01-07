6dc8327a04ca097f0aea232d7e47ae22691b4509
Jack Palfrey <jackgpalfrey@outlook.com>
2024-01-07 18:37:03 +0000

    feat(api): getting all branches now includes more info

--- src/api/routes/branches/index.py
+++ src/api/routes/branches/index.py
@@ -1,13 +1,13 @@
 from flask import render_template
 from src.api.utils.Result import OK
-from src.api.utils.dictify import dictify_simple_branch
+from src.api.utils.dictify import dictify_branch, dictify_simple_branch
 from src.branch.BranchService import BranchService
 
 
 def post():
     branches = BranchService.get_all()
 
-    branches_data = [dictify_simple_branch(b) for b in branches]
+    branches_data = [dictify_branch(b) for b in branches]
 
     return OK({"branches": branches_data})
 
