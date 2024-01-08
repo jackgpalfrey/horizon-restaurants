e693a1f29f53a4b4aea9e15c582f5cd063557ef2
Dina Metwalli <dina.moh657@gmail.com>
2024-01-08 11:38:18 +0000

    refactor(GUI): update main file

--- gui/main.py
+++ gui/main.py
@@ -11,7 +11,7 @@ from Inventory import InventoryPage
 from Table import TablesPage
 from Menu import MenuPage
 from Discounts import DiscountsPage
-# from Reservations import ReservationsPage
+from Reservations import ReservationsPage
 from api import API, URL, State
 
 
@@ -33,32 +33,61 @@ class App(Page):
         user_data_res = API.post(f"{URL}/users/{user}", json=user)
         user_data = user_data_res.json()
         if user_data["success"]:
+            try:
+                self.notebook.forget(0)
+                self.notebook.forget(1)
+                self.notebook.forget(2)
+                self.notebook.forget(3)
+                self.notebook.forget(4)
+                self.notebook.forget(5)
+                self.notebook.forget(6)
+                self.notebook.forget(7)
+                self.notebook.forget(8)
+            except Exception as e:
+                pass
+
             role_id = user_data["data"]["role"]["id"]
+
             match role_id:
                 case 0:
-                    self.notebook.hide(1)
-                    self.notebook.hide(2)
-                    self.notebook.hide(3)
-                    self.notebook.hide(4)
-                    self.notebook.hide(5)
+                    self.notebook.add(self.frame1, text='Home')
+                    self.notebook.add(self.frame7, text='Menu')
+                    self.notebook.add(self.frame8, text='Discounts')
                 case 1:
-                    self.notebook.hide(1)
-                    self.notebook.hide(2)
-                    self.notebook.hide(3)
-                    self.notebook.hide(4)
-                    self.notebook.hide(5)
+                    self.notebook.add(self.frame1, text='Home')
+                    self.notebook.add(self.frame7, text='Menu')
+                    self.notebook.add(self.frame8, text='Discounts')
+                    self.notebook.add(self.frame9, text='Reservations')
                 case 2:
-                    self.notebook.hide(1)
-                    self.notebook.hide(2)
-                    self.notebook.hide(3)
-                    self.notebook.hide(5)
+                    self.notebook.add(self.frame1, text='Home')
+                    self.notebook.add(self.frame7, text='Menu')
+                    self.notebook.add(self.frame5, text='Inventory')
+                    self.notebook.add(self.frame8, text='Discounts')
                 case 3:
-                    self.notebook.hide(1)
-                    self.notebook.hide(2)
-                    self.notebook.hide(3)
-                    self.notebook.hide(5)
+                    self.notebook.add(self.frame1, text='Home')
+                    self.notebook.add(self.frame7, text='Menu')
+                    self.notebook.add(self.frame5, text='Inventory')
+                    self.notebook.add(self.frame8, text='Discounts')
                 case 4:
-                    self.notebook.hide(1)
+                    self.notebook.add(self.frame1, text='Home')
+                    self.notebook.add(self.frame3, text='Cities')
+                    self.notebook.add(self.frame4, text='Branches')
+                    self.notebook.add(self.frame5, text='Inventory')
+                    self.notebook.add(self.frame6, text='Tables')
+                    self.notebook.add(self.frame7, text='Menu')
+                    self.notebook.add(self.frame8, text='Discounts')
+                    self.notebook.add(self.frame9, text='Reservations')
+                case 99:
+                    self.notebook.add(self.frame1, text='Home')
+                    self.notebook.add(self.frame2, text='Staff')
+                    self.notebook.add(self.frame3, text='Cities')
+                    self.notebook.add(self.frame4, text='Branches')
+                    self.notebook.add(self.frame5, text='Inventory')
+                    self.notebook.add(self.frame6, text='Tables')
+                    self.notebook.add(self.frame7, text='Menu')
+                    self.notebook.add(self.frame8, text='Discounts')
+                    self.notebook.add(self.frame9, text='Reservations')
+        State.is_ui_rendered = True
 
     def create_notebook_widget(self):
         style = ttk.Style(self)
@@ -69,26 +98,17 @@ class App(Page):
 
         # create frames
 
-        frame1 = MainTab(self.notebook)
-        frame2 = StaffPage(self.notebook)
-        frame3 = CitiesPage(self.notebook)
-        frame4 = BranchesPage(self.notebook)
-        frame5 = InventoryPage(self.notebook)
-        frame6 = TablesPage(self.notebook)
-        frame7 = MenuPage(self.notebook)
-        frame8 = DiscountsPage(self.notebook)
-        # frame8 = ReservationsPage(self.notebook)
+        self.frame1 = MainTab(self.notebook)
+        self.frame2 = StaffPage(self.notebook)
+        self.frame3 = CitiesPage(self.notebook)
+        self.frame4 = BranchesPage(self.notebook)
+        self.frame5 = InventoryPage(self.notebook)
+        self.frame6 = TablesPage(self.notebook)
+        self.frame7 = MenuPage(self.notebook)
+        self.frame8 = DiscountsPage(self.notebook)
+        self.frame9 = ReservationsPage(self.notebook)
 
         self.notebook.bind("<<NotebookTabChanged>>", self.on_tab_selected)
-        self.notebook.add(frame1, text='Home')
-        self.notebook.add(frame2, text='Staff')
-        self.notebook.add(frame3, text='Cities')
-        self.notebook.add(frame4, text='Branches')
-        self.notebook.add(frame5, text='Inventory')
-        self.notebook.add(frame6, text='Tables')
-        self.notebook.add(frame7, text='Menu')
-        self.notebook.add(frame8, text='Discounts')
-        # self.notebook.add(frame8, text='Reservations')
 
         btn = tk.Button(self, text="Logout",
                         command=lambda: self.pages.goto("loggedout"))
@@ -98,7 +118,7 @@ class App(Page):
         selected_tab = event.widget.select()
         tab_text = event.widget.tab(selected_tab, "text")
 
-        if tab_text == "Home":
+        if not State.is_ui_rendered:
             self.user_view()
 
 
